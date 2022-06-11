2527cb034cb05e273e62ab93b4c2fac08be3ebc6
Morgan DIVERREZ <diverrezm@gmail.com>
2022-06-11 23:54:46 +0200

    debut twig, (reste a tt faire sur l'edit, et le controller a des gros bug)

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -7,19 +7,57 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Formation;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\EntityManagerInterface;
+
+
 
 #[Route('/catalogue', name: 'catalogue_')]
 
 class FormationController extends AbstractController
 {
     #[Route('/', name: 'showAll')]
-    public function showAll(EntityManagerInterface $entityManager): Response
+    public function showAll(EntityManager $formation): Response
     {
-    
-        $entityManager = $this->$this->getDoctrine()->getRepository(Catalogue::class)->findById();
+
+        $formation = $formation->getDoctrine()->getRepository(Catalogue::class)->findById($id);
        
         return $this->render('formation/showAll.html.twig', [
+            'controller_name' => 'FormationController'
+        ]);
+    }
+
+    #[Route('/show/{{formationID}}', name: 'show')]
+    public function show(EntityManagerInterface $entityManager): Response
+    {
+
+        $entityManager = $this->$this->getDoctrine()->getRepository(Catalogue::class)->findById();
+
+        return $this->render('formation/show.html.twig', [
+            'controller_name' => 'FormationController'
+        ]);
+    }
+
+    #[Route('/delete/{{formationID}}', name: 'delete')]
+    public function delete(EntityManagerInterface $entityManager): Response
+    {
+
+        $entityManager = $this->$this->getDoctrine()->getRepository(Catalogue::class)->findById();
+
+        return $this->render('formation/show.html.twig', [
+            'controller_name' => 'FormationController'
+        ]);
+    }
+
+
+    #[Route('/create}', name: 'create')]
+    public function create(EntityManagerInterface $entityManager): Response
+    {
+
+        $entityManager = $this->$this->getDoctrine()->getRepository(Catalogue::class)->findById();
+
+        return $this->render('formation/show.html.twig', [
             'controller_name' => 'FormationController'
         ]);
     }
