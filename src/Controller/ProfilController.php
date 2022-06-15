532df076692de5d79b7c0444a11985aa183216d1
Morgan DIVERREZ <diverrezm@gmail.com>
2022-06-15 23:58:38 +0200

    showAll et show des profils

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -14,11 +14,24 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ProfilController extends AbstractController
 {
-    #[Route('/', name: 'accueil')]
-    public function accueil(EntityManagerInterface $entityManager): Response
+    #[Route('/', name: 'showAll')]
+    public function showAll(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('profil/accueil.html.twig', [
-            'controller_name' => 'ProfilController',
+        $profils = $entityManager->getRepository(Profil::class)->findAll();
+
+        return $this->render('profil/showAll.html.twig', [
+            'profils' => $profils,
+        ]);
+    }
+
+    #[Route('/show/{profilID}', name: 'show')]
+    public function show(EntityManagerInterface $entityManager, $profilID): Response
+    {
+        // find renvoi tjr un array (tableau), donc faut mettre [0] pour enlever l'array, si on veut plus d'une valeur s'il y en a, on met pas ou [nombre]
+        $profil = $entityManager->getRepository(Profil::class)->findById($profilID)[0];
+
+        return $this->render('profil/show.html.twig', [
+            'profil' => $profil,
         ]);
     }
 
@@ -26,19 +39,21 @@ class ProfilController extends AbstractController
     public function edit(EntityManagerInterface $entityManager, Request $request, $profilID): Response
     {
         $profil = $entityManager->getRepository(Profil::class)->findById($profilID)[0];
-        
         $form = $this->createForm(ProfilType::class, $profil);
         $form->handleRequest($request);
 
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $profil ->setLastName(mb_strtoupper($profil->getLastName()));
+            $profil->setLastName(mb_strtoupper($profil->getLastName()));
             $profil->setName(mb_convert_case($profil->getName(), MB_CASE_TITLE, "UTF-8"));
+            //$equipeElus = $entityManager->getRepository(EquipeElu::class)->findById()[0];
+            //$profil->addEquipeElu();
+            //$profil->addAssociation();
 
             $entityManager->persist($profil);
             $entityManager->flush();
-            return $this->redirectToRoute('profil_accueil');
+            return $this->redirectToRoute('profil_showAll');
         }
 
         return $this->render('profil/edit.html.twig', [
