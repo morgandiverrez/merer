8f8c1af76aff9bbd79de25bc735106a32c92cc39
Morgan DIVERREZ <diverrezm@gmail.com>
2022-06-21 00:09:23 +0200

    reglage bdd

--- migrations/Version20220620215810.php
+++ migrations/Version20220620215810.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220614145904 extends AbstractMigration
+final class Version20220620215810 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20220614145904 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE association ADD sigle VARCHAR(255) NOT NULL, CHANGE name name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE association CHANGE sigle sigle VARCHAR(255) DEFAULT NULL, CHANGE extension image VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE badge DROP extension');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE association DROP sigle, CHANGE name name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE association CHANGE sigle sigle VARCHAR(255) NOT NULL, CHANGE image extension VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE badge ADD extension VARCHAR(255) DEFAULT NULL');
     }
 }
