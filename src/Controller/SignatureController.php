530ea366ebd34f8ef267e45fcbe62177bae9d3db
Morgan DIVERREZ <diverrezm@gmail.com>
2022-07-05 22:08:52 +0200

    reglage signature mail

--- src/Controller/SignatureController.php
+++ src/Controller/SignatureController.php
@@ -3,21 +3,19 @@
 namespace App\Controller;
 
 
-use App\Entity\Profil;
-use App\Entity\Association;
-use App\Form\AssociationType;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
+
+
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
 
 
 #[Route('/signature', name: 'signature_')]
 
-class AssociationController extends AbstractController
+class SignatureController extends AbstractController
 {
     #[Route('/CROUS', name: 'CROUS')]
     #[IsGranted('ROLE_USER')]
@@ -32,4 +30,12 @@ class AssociationController extends AbstractController
     {
         return $this->render('signature/centrauxUBO.html.twig');
     }
+
+    #[Route('/BF', name: 'BF')]
+    #[IsGranted('ROLE_BF')]
+    public function BF(): Response
+    {
+        return $this->render('signature/BF.html.twig');
+    }
 }
+
