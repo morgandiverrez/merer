b1ae0b4e0a59283774b4ad3533300da98f173980
Diverrez morgan <97022189+morgandiverrez@users.noreply.github.com>
2023-03-22 15:05:32 +0100

    adaptation pour repayGrid 2023

---
+++ migrations/Version20230322134613.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230322134613 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE repay_grid ADD coef INT NOT NULL, DROP distance, CHANGE travel_mean caracteristique VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE repay_grid ADD distance DOUBLE PRECISION NOT NULL, DROP coef, CHANGE caracteristique travel_mean VARCHAR(255) NOT NULL');
+    }
+}
